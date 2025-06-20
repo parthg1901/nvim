9bbc6b7f5f17586f2f32ab407850478012b64e1f
Parth Gupta <parth.eng1210@gmail.com>
2025-06-20 16:10:29 +0530

    add todo-comments keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local km = vim.keymap.set
+km("n", "]t", function()
+  require("todo-comments").jump_next()
+end, { desc = "Next todo comment" })
+km("n", "[t", function()
+  require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
+km("n", "<leader>st", "<cmd>TodoTelescope<cr>", { desc = "Show all todos (Telescope)" })
+km("n", "<leader>xt", "<cmd>TodoTrouble<cr>", { desc = "Show todos (Trouble)" })
